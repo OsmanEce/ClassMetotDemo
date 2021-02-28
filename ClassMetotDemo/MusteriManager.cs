f38dd9423569c92ad4c4ae710ddb7b4a7cc0e1de
Osman Eceoğlu <osmaneceoglu@akdeniz.edu.tr>
2021-02-28 15:36:58 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi : " + musteri.Adi +" "+ musteri.Soyadi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+
+            Console.WriteLine("Müşteri Listesi : " + musteri.Adi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Osman";
+            musteri1.Soyadi = "ECEOĞLU";
+            musteri1.Yasi = 35;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Hakan";
+            musteri2.Soyadi = "DEMİR";
+            musteri2.Yasi = 40;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+
+            Console.WriteLine("--------Müşteri silme işlemi------------");
+
+            musteriManager.Sil(musteri2);
+
+
+            Console.WriteLine("----------Müşteri Listesi----------");
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.Listele(musteri);
+            }
+
+
+
+        }
+    }
+}
